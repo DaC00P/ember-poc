ce3118d64aa8f3ec59b69db73a9b52aab183f125
Coop <dacooperfish@gmail.com>
2021-05-13 18:55:39 -0700

    sub-route for booking added

--- app/router.js
+++ app/router.js
@@ -7,5 +7,7 @@ export default class Router extends EmberRouter {
 }
 
 Router.map(function () {
-  this.route('doctor');
+  this.route('doctor', function () {
+    this.route('book', { path: '/:id' });
+  });
 });
